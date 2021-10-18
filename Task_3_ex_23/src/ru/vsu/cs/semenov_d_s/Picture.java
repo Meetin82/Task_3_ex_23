a640932a167cfef7015d43fcd36184310da485bf
Meetin82 <90614890+Meetin82@users.noreply.github.com>
2021-10-19 00:49:57 +0300

    Add files via upload

---
+++ Task_3_ex_23/src/ru/vsu/cs/semenov_d_s/Picture.java
@@ -0,0 +1,35 @@
+package ru.vsu.cs.semenov_d_s;
+
+import ru.vsu.cs.semenov_d_s.Figures.Line;
+import ru.vsu.cs.semenov_d_s.Figures.Rectangle;
+import ru.vsu.cs.semenov_d_s.Figures.VerticalParabola;
+import java.awt.geom.Point2D;
+
+public class Picture {
+
+    public final VerticalParabola P = new VerticalParabola(0, -6, 0.25);
+    public final Rectangle R = new Rectangle(new Point2D.Double(-6, -2), new Point2D.Double(-1, 4));
+    public final Line L = new Line(-1, 2, -2);
+
+    public SimpleColor getColor(double x, double y){
+        if (L.isPointAboveLine(x, y) && P.isPointAboveParabola(x, y) && !R.isPointInsideRectangle(x, y)) {
+            return SimpleColor.YELLOW;
+
+        } else if (R.isPointInsideRectangle(x, y) && L.isPointAboveLine(x, y))
+            return SimpleColor.ORANGE;
+
+        else if (L.isPointAboveLine(x, y) && !P.isPointAboveParabola(x, y)) {
+            return SimpleColor.GRAY;
+
+        } else if (!L.isPointAboveLine(x, y) && y > 4)
+            return SimpleColor.BLUE;
+
+        else if (!P.isPointAboveParabola(x, y) && !L.isPointAboveLine(x, y)) {
+            return SimpleColor.BLUE;
+        } else {
+            return SimpleColor.GRAY;
+        }
+    }
+
+}
+
