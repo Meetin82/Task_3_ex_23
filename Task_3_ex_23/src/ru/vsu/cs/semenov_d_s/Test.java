5b3e3f90abe6059cc71d932cac35eed74fa27b72
Meetin82 <90614890+Meetin82@users.noreply.github.com>
2021-10-27 14:48:32 +0300

    Update Test.java

--- Task_3_ex_23/src/ru/vsu/cs/semenov_d_s/Test.java
+++ Task_3_ex_23/src/ru/vsu/cs/semenov_d_s/Test.java
@@ -6,17 +6,17 @@ import static ru.vsu.cs.semenov_d_s.Main.printColor;
 public class Test {
 
     public static boolean makeTest() {
-        if (picture.getColor(3,4) != SimpleColor.YELLOW){
+        if (picture.getColor(3, 4) != SimpleColor.YELLOW){
             return false;
-        } else if (picture.getColor(0,-2) != SimpleColor.GRAY){
+        } else if (picture.getColor(0, -2) != SimpleColor.GRAY){
             return false;
-        } else if (picture.getColor(-1.5,3.5) != SimpleColor.ORANGE){
+        } else if (picture.getColor(-1.5, 3.5) != SimpleColor.ORANGE){
             return false;
-        } else if (picture.getColor(-4,5) != SimpleColor.BLUE){
+        } else if (picture.getColor(-4, 5) != SimpleColor.BLUE){
             return false;
-        } else if (picture.getColor(-9,1) != SimpleColor.BLUE){
+        } else if (picture.getColor(-9, 1) != SimpleColor.BLUE){
             return false;
-        } else if (picture.getColor(8,-3) != SimpleColor.GRAY){
+        } else if (picture.getColor(8, -3) != SimpleColor.GRAY){
             return false;
         }
         else {
@@ -25,12 +25,12 @@ public class Test {
     }
 
     public static void printTest(){
-        printColor(3,4);
-        printColor(0,-2);
+        printColor(3, 4);
+        printColor(0, -2);
         printColor(-1.5, 3.5);
         printColor(-4, 5);
         printColor(-9, 1);
-        printColor(8,-3);
+        printColor(8, -3);
     }
 
 }
